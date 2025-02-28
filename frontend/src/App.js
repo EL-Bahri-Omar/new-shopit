01a5f396e09c5787573cb8918cd4682ee2ad3239
omar bahri <bahri.omar69@gmail.com>
2025-02-28 13:51:18 +0100

    Adding Filter, Pagination, Search

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Home from './components/Home';
+import ProductDetails from "./components/product/ProductDetails";
+
+import Login from "./components/user/Login";
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          <Routes>
+            <Route path="/" Component={Home} exact />
+            <Route path="/search/:keyword" Component={Home} />
+            <Route path="/product/:id" Component={ProductDetails} exact />
+            
+            <Route path="/login" Component={Login} /> 
+
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+
+import {
+    All_PRODUCTS_REQUEST,
+    All_PRODUCTS_SUCCESS,
+    All_PRODUCTS_FAIL,
+    PRODUCTS_DETAILS_REQUEST,
+    PRODUCTS_DETAILS_SUCCESS,
+    PRODUCTS_DETAILS_FAIL,
+    CLEAR_ERRORS
+} from '../constants/productConstants';
+
+export const getProducts = (keyword = '', currentPage = 1, price, category, rating = 0) => async (dispatch) =>{
+    try {
+        dispatch({ type: All_PRODUCTS_REQUEST })
+
+        let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&ratings[gte]=${rating}`;
+        
+        if (category) {
+            link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&category=${category}&ratings[gte]=${rating}`;
+        }
+        const { data } = await axios.get(link);
+
+        dispatch({
+            type: All_PRODUCTS_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: All_PRODUCTS_FAIL,
+            payload: error.response.data.message
+        })    
+    }
+
+}
+
+export const getProductDetails = (id) => async (dispatch) =>{
+    try {
+        dispatch({ type: PRODUCTS_DETAILS_REQUEST })
+        const { data } = await axios.get(`/api/v1/product/${id}`)
+        dispatch({
+            type: PRODUCTS_DETAILS_SUCCESS,
+            payload: data.product
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCTS_DETAILS_FAIL,
+            payload: error.response.data.message
+        })    
+    }
+
+}
+
+//clear errors
+export const clearErrors = () => async(dispatch) =>{
+    dispatch({
+        type:CLEAR_ERRORS
+    })
+}
